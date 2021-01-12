9cae217b22f2234ffc3a891cb1688ab64e0b3ded
Cesar Amaya <Ceschom1010@hotmail.com>
2021-01-12 12:32:13 -0600

    arreglo del menu

--- src/Components/inicio.js
+++ src/Components/inicio.js
@@ -5,10 +5,8 @@ import Grid from '@material-ui/core/Grid'
 export default function Inicio() {
     return(
         <div className="ini">
-            <Grid>
             <h1>BIENVENIDOS AL SITIO</h1>
             <img src={inicioo} className="inicio"/>
-            </Grid>
         </div>
            
     )
--- src/Containers/Routing/Menu.js
+++ src/Containers/Routing/Menu.js
@@ -7,16 +7,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from '@material-ui/icons/Home';
-import { green } from '@material-ui/core/colors';
 import PersonPinIcon from '@material-ui/icons/PersonPin';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: theme.spacing(12),
+    height: theme.spacing(14),
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
   },
 }));
 
@@ -27,15 +26,15 @@ function Menu() {
       <Grid container spacing={1}>
         <Grid xs={12}>
           <AppBar>
-          <Typography variant="h6" className={classes.root} align="left" >
+          <Typography variant="h7" className={classes.root} align="left" >
                 <Link href="#/" color="inherit" className={classes.paper}>
-                {<HomeIcon fontSize="large"/>}Inicio
+                Inicio{<HomeIcon />}
                 </Link>
                 <Link href="#/empleados" color="inherit" className={classes.paper} >
-                  {<PersonPinIcon fontSize="large"/>}Empleados
+                  Empleados{<PersonPinIcon />}
                 </Link>
                 <Link href="#/incapacidades" color="inherit" className={classes.paper}>
-                  {<LocalHospitalIcon fontSize="large"/>}Incapacidades
+                Incapacidades{<LocalHospitalIcon />}
                 </Link>
                 <LoginButton />
                 <LogoutButton />
